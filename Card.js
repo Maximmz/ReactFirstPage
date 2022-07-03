1702b04832e821c83431a3804a88892a6039f4c2
Maximmz <108608264+Maximmz@users.noreply.github.com>
2022-07-03 13:20:06 +0500

    Add files via upload

---
+++ Card.js
@@ -0,0 +1,9 @@
+import './Card.css';
+
+function Card(props)
+ {
+    const classes = 'card ' + props.className;
+    return <div classname={classes}>{props.children}</div> ;
+}
+ 
+export default Card;
